f032ad81781787adcc697774e8f9e951cb2fb662
matiyorke <cortesgmat@gmail.com>
2026-04-26 20:52:55 -0300

    primer commit de prueba. Cree el primer webform

---
+++ TP4_Progra_II/WebApplication1/Ejercicio1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="WebApplication1.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblDestinoInicio" runat="server" Font-Underline="True" Text="DESTINO INICIO: "></asp:Label>
+            <br />
+            <br />
+        </div>
+        <asp:Label ID="lblProvincia1" runat="server" Font-Bold="True" Text="PROVINCIA: "></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlProvincia1" runat="server">
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="lblLocalidad1" runat="server" Font-Bold="True" Text="LOCALIDAD:"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlLocalidad1" runat="server">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="lblDestinoFinal" runat="server" Font-Underline="True" Text="DESTINO FINAL:"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="lblProvincia2" runat="server" Font-Bold="True" Text="PROVINCIA:"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlProvincia2" runat="server">
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="lblLocalidad2" runat="server" Font-Bold="True" Text="LOCALIDAD:"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlLocalidad2" runat="server">
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ TP4_Progra_II/WebApplication1/Ejercicio1.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1
+{
+
+
+    public partial class Ejercicio1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblDestinoInicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDestinoInicio;
+
+        /// <summary>
+        /// Control lblProvincia1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblProvincia1;
+
+        /// <summary>
+        /// Control ddlProvincia1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlProvincia1;
+
+        /// <summary>
+        /// Control lblLocalidad1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLocalidad1;
+
+        /// <summary>
+        /// Control ddlLocalidad1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlLocalidad1;
+
+        /// <summary>
+        /// Control lblDestinoFinal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDestinoFinal;
+
+        /// <summary>
+        /// Control lblProvincia2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblProvincia2;
+
+        /// <summary>
+        /// Control ddlProvincia2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlProvincia2;
+
+        /// <summary>
+        /// Control lblLocalidad2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLocalidad2;
+
+        /// <summary>
+        /// Control ddlLocalidad2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlLocalidad2;
+    }
+}
