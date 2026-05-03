a01c4d86ab31f598e0e6415659b3083bc428b6cc
Tomas Torregiani <tomitorre91@gmail.com>
2026-05-03 17:40:02 -0300

    Optimice codigo repetido con DataTable y Session. Cree funcion ObtenerProductos

--- TP4_Progra_II/WebApplication1/Ejercicio2.aspx.cs
+++ TP4_Progra_II/WebApplication1/Ejercicio2.aspx.cs
@@ -13,11 +13,24 @@ namespace WebApplication1
     public partial class Ejercicio2 : System.Web.UI.Page
     {
         private string rutaNeptunoSQL = @"Data Source=.\SQLEXPRESS;Initial Catalog=Neptuno; Integrated Security=True; Encrypt=False;TrustServerCertificate=True";
-
+        private DataTable ObtenerProductos()
+        {
+            using (SqlConnection conn = new SqlConnection(rutaNeptunoSQL))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Productos", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+           
             if (!IsPostBack)
             {
+                DataTable dt = ObtenerProductos(); //Llamamos a la funcion obtener productos
+                Session["Productos"] = dt;//Almacenamos
+
                 CargarGrid();
             }
         }
@@ -25,18 +38,14 @@ namespace WebApplication1
         // Método para traer todos los datos
         private void CargarGrid()
         {
-            SqlConnection conn = new SqlConnection(rutaNeptunoSQL);
+            /*SqlConnection conn = new SqlConnection(rutaNeptunoSQL);
             conn.Open();
-
             SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Productos", conn);
             DataTable dt = new DataTable();
-
-            da.Fill(dt);
-
-            gvProdCat.DataSource = dt;
+            da.Fill(dt);*/
+            gvProdCat.DataSource = Session["Productos"]; //Utilizamos los datos almacenados en Session
             gvProdCat.DataBind();
-
-            conn.Close();
+            //conn.Close();
         }
 
 
@@ -50,18 +59,19 @@ namespace WebApplication1
             else
             {
 
-                SqlConnection conn = new SqlConnection(rutaNeptunoSQL);
-                conn.Open();
+                /*SqlConnection conn = new SqlConnection(rutaNeptunoSQL);
+                conn.Open();*/
 
                 string[] ops = { "=", ">", "<" };
                 string operadorP = ops[int.Parse(DropDownList1.SelectedValue)]; //operador en producto
                 string operadorC = ops[int.Parse(DropDownList2.SelectedValue)]; //operador en categoría
                 bool productoF = txtProducto.Text != "";  //tiene producto o no
                 bool categoriaF = txtCategoria.Text != "";  //tiene categoría o no
-                string consulta = "";
+                                                            //string consulta = "";
+
 
                 //caso completo//
-                if (productoF && categoriaF)
+                /*if (productoF && categoriaF)
                 {
                     consulta = "SELECT * FROM Productos WHERE IdProducto " + operadorP + " @id AND IdCategoría " + operadorC + " @cat";
                 }
@@ -79,7 +89,7 @@ namespace WebApplication1
                 }
 
 
-                SqlCommand cmd = new SqlCommand(consulta, conn);
+                //SqlCommand cmd = new SqlCommand(consulta, conn);
 
                 // Convertir a número
                 if (categoriaF)
@@ -91,24 +101,39 @@ namespace WebApplication1
                     cmd.Parameters.AddWithValue("@id", int.Parse(txtProducto.Text));
 
                 }
-
-
-
-
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable();
 
-                da.Fill(dt);
+                da.Fill(dt);*/
 
-                gvProdCat.DataSource = dt;
+                DataTable dt = (DataTable)Session["Productos"]; //Traemos los datos almacenados en Session
+                DataView dv = new DataView(dt); //Cargamos en DataView lo que esta en la DataTable
+
+                string filtro = ""; //Creamos un query dinamico
+                if (productoF)
+                {
+                    filtro += $"IdProducto {operadorP} {txtProducto.Text}"; //Si hay producto se agrega
+                }
+                if (categoriaF)
+                {
+                    if(filtro != "")
+                    {
+                        filtro += " AND "; //Si hay producto y catergoria, agrega un AND para conectar
+                    }
+                    filtro += $"IdCategoría {operadorC} {txtCategoria.Text}"; //Agrega despues del AND para completar la consulta
+                }
+                //Aplicamos el filtro
+                dv.RowFilter = filtro;
+                //bindeamos
+                gvProdCat.DataSource = dv;
                 gvProdCat.DataBind();
 
                 txtProducto.Text = ""; // limpiamos los campos
                 txtCategoria.Text = "";
 
-                lblResultados.Text = "Resultados encontrados: " + dt.Rows.Count; // mostramos el número de resultados encontrados
+                lblResultados.Text = "Resultados encontrados: " + dv.Count; // mostramos el número de resultados encontrados
 
-                conn.Close();
+                //conn.Close();
             }
         }
 
